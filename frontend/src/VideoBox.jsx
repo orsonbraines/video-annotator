863faa3ccaa1c1d9a0ed26efcf4b411650a0c7c8
Orson Baines <obraines@gmail.com>
2021-09-18 03:28:23 -0400

    front-end files

---
+++ frontend/src/VideoBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './VideoBox.css';
+
+export default function VideoBox() {
+  return (
+    <div className='videoContainer'>
+      This is the video div.
+    </div>
+  );
+}
