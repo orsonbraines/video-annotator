a486c1116cc3c1cbc887de738aa1fdc3cc90392d
Tony Sun <sunwuyue@live.com>
2021-09-18 19:38:31 -0700

    CSS button changes

--- frontend/src/Transcript.jsx
+++ frontend/src/Transcript.jsx
@@ -4,7 +4,7 @@ import './Transcript.css';
 export default function Transcript(props) {
   return (
     <div className='transcript' style={props.highlighted?{'backgroundColor':'yellow'} : {}}>
-      <span><button onClick={() => props.setSeekTime(props.transcript.ts / 1000.0)}>{`${String(Math.floor(props.transcript.ts / 60000)).padStart(2,'0')}:${String(Math.floor((props.transcript.ts - 60000 * Math.floor(props.transcript.ts / 60000)) / 1000)).padStart(2,'0')}`}</button></span>
+      <span><button class="ts-btn" onClick={() => props.setSeekTime(props.transcript.ts / 1000.0)}>{`${String(Math.floor(props.transcript.ts / 60000)).padStart(2,'0')}:${String(Math.floor((props.transcript.ts - 60000 * Math.floor(props.transcript.ts / 60000)) / 1000)).padStart(2,'0')}`}</button></span>
       <span className="transcriptTxt">{props.transcript.txt}</span>
     </div>
   );
