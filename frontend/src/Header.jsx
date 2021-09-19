91ed4d8095ec4054e45a1e459e2d8eedae0ea678
jordanmao <jordanmao2@gmail.com>
2021-09-18 19:07:12 -0700

    made header a link

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div id='header'>
-      <h1 id='appName'>Video Annotator App</h1>
+      <Link to = "/">
+        <h1 id='appName'>Video Annotator App</h1>
+      </Link>
     </div>
   );
 }
